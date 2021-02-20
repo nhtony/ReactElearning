b216cbf36bc6953e006db1b8b02e13c491f17c87
Hao Ho <honhathao062@gmail.com>
2021-02-20 19:12:36 +0700

    Updated package.json

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import thunk from 'redux-thunk';
 //import store redux
 import rootReducer from './redux/index';
 
-const store = createStore(rootReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer,compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
